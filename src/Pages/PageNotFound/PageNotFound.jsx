1dbed749e0b4deb2843348ad2ea8ae4b7f25a6ac
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-06-04 16:45:24 +0530

    pagenotfound

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styles from "./PageNotFound.module.css";
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
+
+  const {app__error, error__code, error__msg} = styles;
+
   return (
-    <div>PageNotFound</div>
-  )
+    <div className={app__error}>
+      <p className={error__code}>404</p>
+      <p className={error__msg}>OOPS! Page Not Found.</p>
+      <Link to="/">
+        <ion-icon name="chevron-back"></ion-icon> <span>Back Home</span>
+      </Link>
+    </div>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
